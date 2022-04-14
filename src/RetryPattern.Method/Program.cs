f9b4cc07a57d5aacec57b06e37a419b2f0f6b785
Felipe Augusto <felipementel@hotmail.com>
2022-04-14 16:34:25 -0300

    update to Async methods

--- src/RetryPattern.Method/Program.cs
+++ src/RetryPattern.Method/Program.cs
@@ -4,52 +4,58 @@ namespace RetryPattern.Method
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             int retryAttempts = 5;
+
             var retry = Policy
             .Handle<Exception>()
-            .WaitAndRetry(
+            .Or<ArgumentException>()
+            .WaitAndRetryAsync(
                 retryCount: retryAttempts,
                 sleepDurationProvider: retryAttempt =>
                 TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)),
                 onRetry: (exception, sleepDuration, attemptNumber, context) =>
-            {
-                Console.WriteLine($"Retrying in {sleepDuration}. {attemptNumber}" +
+                {
+                    Console.WriteLine($"Retrying in {sleepDuration}. {attemptNumber}" +
                     $" / {retryAttempts}. Error message: {exception.Message}");
-            });
+                });
 
             try
             {
-                retry.Execute(() =>
+                await retry.ExecuteAsync(async () =>
                 {
                     ClientService clientService = new();
-                    clientService.Login();
+                    await clientService.Login();
 
+                    Console.WriteLine(Environment.NewLine);
                     Console.WriteLine("You are Authenticated ! ! !");
+                    Console.WriteLine(Environment.NewLine);
 
                     Console.WriteLine("Press any key to continue");
                     Console.ReadKey();
                 });
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
-            }            
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
         }
     }
 
     public class ClientService
     {
-        public bool Login()
+        public async Task<bool> Login()
         {
-            Random random = new();
-            int rNumber = random.Next(int.MinValue,int.MaxValue);
+            return await Task.Factory.StartNew(() =>
+            {
+                Random random = new();
+                int rNumber = random.Next(int.MinValue, int.MaxValue);
 
-            return rNumber % 2 == 0 
-                ? true 
-                : throw new Exception($"Login error. Random number was {rNumber}! ! !");
+                return rNumber % 2 == 0
+                    ? true
+                    : throw new Exception($"Login error. Random number was {rNumber}! ! !");
+            });
         }
     }
 }
